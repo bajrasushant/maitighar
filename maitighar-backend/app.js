a71b46bd10294965046b89e87a02df404fd728c5
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-07-20 13:40:54 +0545

    user login and register

--- maitighar-backend/app.js
+++ maitighar-backend/app.js
@@ -4,6 +4,8 @@ const config = require("./utils/config");
 const app = express();
 const mongoose = require("mongoose");
 const issueRouter = require("./controllers/issues")
+const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -14,6 +16,10 @@ mongoose
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
+
 
 app.get("/", (request, response) => {
 	response.send("<h1>Hello World!</h1>");
---
+++ maitighar-backend/controllers/users.js
@@ -0,0 +1,62 @@
+const bcrypt = require("bcrypt");
+const userRouter = require("express").Router();
+const User = require("../models/user");
+
+userRouter.get("/", async (request, response) => {
+	const users = await User.find({});
+	response.json(users);
+});
+
+userRouter.post("/", async (request, response) => {
+	const { username, password, repassword, email } = request.body;
+	if (
+		!username ||
+		!password ||
+		!repassword ||
+		username.trim() === "" ||
+		password.trim() === "" ||
+		repassword.trim() === ""
+	) {
+		return response
+			.status(400)
+			.json({ error: "username, password, and repassword are required" });
+	}
+
+	if (!(username.length >= 3 && password.length >= 3)) {
+		return response.status(400).json({
+			error: "username and password should contain at least 3 characters",
+		});
+	}
+
+	if (password !== repassword) {
+		return response.status(400).json({ error: "passwords do not match" });
+	}
+
+	const saltRounds = 10;
+	const passwordHash = await bcrypt.hash(password, saltRounds);
+
+	const user = new User({
+		username,
+		passwordHash,
+		email,
+		role: "citizen",
+	});
+
+	try {
+		const savedUser = await user.save();
+		return response.status(201).json(savedUser);
+	} catch (error) {
+		if (error.name === "ValidationError") {
+			return response.status(400).json({ error: error.message });
+		}
+		if (error.code === 11000) {
+			// Duplicate key error
+			return response.status(400).json({
+				error: "Username or email already exists",
+			});
+		}
+		return response.status(500).json({ error: "Something went wrong" });
+	}
+});
+
+module.exports = userRouter;
