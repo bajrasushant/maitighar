853e390f20efe77f0f81f172092bc2cbf33225d2
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-07-20 18:06:08 +0545

    register logout integration

---
+++ maitighar-frontend/src/services/user.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const baseUrl = "/api/users";
+
+export const signUp = async (newUserDetail) => {
+  try {
+    const response = await axios.post(baseUrl, newUserDetail);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
