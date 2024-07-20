abfc935f36ab4d63a10be9a92346dca419d9a546
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-07-20 12:13:38 +0545

    mongo connect

--- maitighar-backend/app.js
+++ maitighar-backend/app.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const config = require("./utils/config");
+const app = express();
+const mongoose = require("mongoose");
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+mongoose.set("strictQuery", false);
+const uri = "mongodb+srv://bajrasushant:3RiHt10neDtkw2G3@maitighar.8hcenvt.mongodb.net/maitighar?retryWrites=true&w=majority&appName=maitighar"
+mongoose
+	.connect(uri)
+	.then(() => console.log("connected to Mongo"))
+	.catch((err) => console.log("error connecting", err.message));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+	response.send("<h1>Hello World!</h1>");
+});
 
 module.exports = app;
