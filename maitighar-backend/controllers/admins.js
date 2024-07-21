9c9ee014d1c9a6bbecea9ccb42781553a8340e9d
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-07-20 23:27:56 -0700

    chore: Update departments list with Ward numbers

--- maitighar-backend/controllers/admins.js
+++ maitighar-backend/controllers/admins.js
@@ -2,7 +2,40 @@ const bcrypt = require("bcrypt");
 const adminRouter = require("express").Router();
 const Admin = require("../models/admin");
 
-const departments = ["roads", "water", "education", "garbage", "health"];
+const departments = [
+  "Ward No.1",
+  "Ward No.2",
+  "Ward No.3",
+  "Ward No.4",
+  "Ward No.5",
+  "Ward No.6",
+  "Ward No.7",
+  "Ward No.8",
+  "Ward No.9",
+  "Ward No.10",
+  "Ward No.11",
+  "Ward No.12",
+  "Ward No.13",
+  "Ward No.14",
+  "Ward No.15",
+  "Ward No.16",
+  "Ward No.17",
+  "Ward No.18",
+  "Ward No.19",
+  "Ward No.20",
+  "Ward No.21",
+  "Ward No.22",
+  "Ward No.23",
+  "Ward No.24",
+  "Ward No.25",
+  "Ward No.26",
+  "Ward No.27",
+  "Ward No.28",
+  "Ward No.29",
+  "Ward No.30",
+  "Ward No.31",
+  "Ward No.32",
+];
 
 adminRouter.get("/", async (request, response) => {
     const users = await Admin.find({});
