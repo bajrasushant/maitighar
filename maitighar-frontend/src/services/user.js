3fc8d9d83bdf3ddf6696c857a6996df1f8490b22
UNDERDROID <roshan.stha399@gmail.com>
2024-10-17 22:33:11 +0545

    resend OTP

--- maitighar-frontend/src/services/user.js
+++ maitighar-frontend/src/services/user.js
@@ -12,12 +12,22 @@ export const signUp = async (newUserDetail) => {
   }
 };
 
-// New function for OTP verification
+// OTP verification function
 export const verifyOtp = async ({ email, otp }) => {
   try {
     const response = await axios.post(`${baseUrl}/verify-otp`, { email, otp });
     return response.data; // Return the response data if needed
   } catch (error) {
     throw error.response.data; // Throw the error if something goes wrong
+  }
+};
+
+// Resend OTP function
+export const resendOtp = async ({ email }) => {
+  try {
+    const response = await axios.post(`${baseUrl}/resend-otp`, { email });
+    return response.data; // Return response data for success message
+  } catch (error) {
+    throw error.response.data; // Throw error in case of failure
   }
 };
