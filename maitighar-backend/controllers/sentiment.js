5d8c2846de68062a88dfb25431c61592df4368a8
Yakumaa <maharjanshrish8@gmail.com>
2024-10-25 21:25:38 +0545

    backend integration test (not working)

---
+++ maitighar-backend/controllers/sentiment.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+const Issue = require("../models/issue");
+const Comment = require("../models/comment");
+
+async function analyzeSentiment(postId) {
+  try {
+    // Get post and comments from MongoDB
+    const post = await Issue.findById(postId);
+    const comments = await Comment.find({ postId });
+
+    // Call Flask API
+    const response = await axios.post("http://localhost:5000/analyze", {
+      post_description: post.description,
+      comments: comments.map((c) => c.text),
+    });
+
+    // Update post with sentiment analysis results
+    await Issue.findByIdAndUpdate(postId, {
+      sentiment: response.data.overall_sentiment,
+      sentimentScore: response.data.average_score,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Sentiment analysis failed:", error);
+    throw error;
+  }
+}
+
+module.exports = { analyzeSentiment };
